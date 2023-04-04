9f761e37cd60c3681f3ce90d3f1815689aa7182c
e-nagornyak <dolkiv@gmail.com>
2023-04-05 02:46:49 +0300

    Added empty icon

--- src/features/table-wrapper/table/Table.tsx
+++ src/features/table-wrapper/table/Table.tsx
@@ -1,8 +1,8 @@
 import React, { FC, memo } from 'react'
 import s from './Table.module.scss'
+import empty from 'assept/images/empty.png'
 import { TableButton } from 'common'
-import { TableHead } from 'features/table-wrapper/table/table-head/TableHead'
-import { COLUMNS, RowsType } from 'features/table-wrapper/Table-constants'
+import { COLUMNS, RowsType, TableHead } from 'features'
 
 type PropsType = {
   rows: RowsType[]
@@ -11,29 +11,33 @@ type PropsType = {
 export const Table: FC<PropsType> = memo(({ rows }) => {
   return (
     <div>
-      <table className={s.tableWithGrid}>
-        <thead>
-          <tr>
-            {COLUMNS.map(c => (
-              <TableHead title={c.title} key={c.id} />
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {rows.map(r => (
-            <tr key={r.id}>
-              <td>{r.name}</td>
-              <td>{r.company}</td>
-              <td>{r.phone}</td>
-              <td>{r.email}</td>
-              <td>{r.country}</td>
-              <td>
-                <TableButton title={r.status} />
-              </td>
+      {rows.length ? (
+        <table className={s.tableWithGrid}>
+          <thead>
+            <tr>
+              {COLUMNS.map(c => (
+                <TableHead title={c.title} key={c.id} />
+              ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {rows.map(r => (
+              <tr key={r.id}>
+                <td>{r.name}</td>
+                <td>{r.company}</td>
+                <td>{r.phone}</td>
+                <td>{r.email}</td>
+                <td>{r.country}</td>
+                <td>
+                  <TableButton title={r.status} />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <img className={s.empty_img} src={empty} alt="empty" />
+      )}
     </div>
   )
 })
